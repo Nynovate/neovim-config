15ceffdf31dd7c81182c6adb151310d4cb705bd6
Ny Hasina <djazejhasi@gmail.com>
2025-03-16 14:10:20 +0300

    The terminal now will occupy 1/3 of the screen, padded to the right of the screen

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,8 +3,12 @@ return
 	'akinsho/toggleterm.nvim',
 	config = function()
 			local	toggleterm = require("toggleterm");
+			local	win_width = math.floor(vim.api.nvim_win_get_width(0) / 3);
+			local	win_height = math.floor(vim.api.nvim_win_get_height(0));
+			local	screen_padding = 5
 
 			toggleterm.setup({
+				size = 10,
 				display_name = "Terminal",
 				open_mapping = [[<S-TAB>]],
 				direction = 'float',
@@ -14,6 +18,10 @@ return
 					}
 				},
 				float_opts = {
+					width = win_width,
+					height = win_height - screen_padding,
+					col = vim.api.nvim_win_get_width(0) - win_width - screen_padding,
+					row = screen_padding / 2,
 					border = 'curved'
 				},
 				shell = 'zsh',
