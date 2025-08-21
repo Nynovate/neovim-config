f3bb6a5762218414258eeef3267a127f374ab499
Ny Hasina <djazejhasi@gmail.com>
2025-08-21 14:37:11 +0300

    update: the previous version break the lsp.

--- lua/custom/plugins/lsp/mason.lua
+++ lua/custom/plugins/lsp/mason.lua
@@ -1,42 +1,41 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
+  "williamboman/mason.nvim",
+  version = "^1.0.0",  -- lock mason.nvim to v1.x
+  dependencies = {
+    { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" }, -- lock mason-lspconfig.nvim to v1.x
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
 
-	config = function()
-		local mason = require("mason")
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		local mason_lspconfig = require("mason-lspconfig")
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = " ",
+          package_pending = "󰦗 ",
+          package_uninstalled = " ",
+        },
+        border = "rounded",
+        width = 0.9,
+        height = 0.8,
+      },
+    })
 
-		local mason_tool_installer = require("mason-tool-installer")
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+      },
+      automatic_installation = false, -- explicitly disable automatic install to avoid conflicts
+    })
 
-
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = " ",
-					package_pending = "󰦗 ",
-					package_uninstalled = " ",
-				},
-				border = "rounded",
-				width = 0.9,
-				height = 0.8,
-			},
-		})
-
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"lua_ls",
-				"clangd",
-			},
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"stylua",
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "stylua",
+      },
+    })
+  end,
 }
