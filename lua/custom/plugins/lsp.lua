9e39f6c5ad298be5f13e5da1041c0a76e91d1ad3
Ny Hasina <djazejhasi@gmail.com>
2025-08-28 15:36:34 +0300

    update: add a nice rounded border to Mason.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -9,12 +9,22 @@ return {
 		},
 		config = function()
 			-- Setup mason
-			require("mason").setup()
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+					icons = {
+						package_installed = "",
+						package_pending = "󰦗 ",
+						package_uninstalled = "✗"
+					}
+				}
+			})
 
 			-- Ensure clangd is installed and auto-installed if missing
 			require("mason-lspconfig").setup({
 				ensure_installed = { "clangd" },
 				automatic_installation = true,
+
 			})
 
 			-- Get nvim-cmp capabilities
