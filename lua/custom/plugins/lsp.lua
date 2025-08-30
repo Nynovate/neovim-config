b23e1ecfd77e9157b71d7d887a7f64d197a1ee76
Ny Hasina <djazejhasi@gmail.com>
2025-08-30 16:30:52 +0300

    update: lsp keymaps!

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -27,6 +27,57 @@ return {
 
 			})
 
+			local keymap = vim.keymap
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = {
+						buffer = ev.buf,
+						silent = false,
+					}
+
+					opts.desc = "Show LSP references"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+					opts.desc = "Go to declaration"
+					keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+					opts.desc = "See available code actions"
+					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+					opts.desc = "Smart rename"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+					opts.desc = "Show buffer diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+					opts.desc = "Show line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+					opts.desc = "Go to previous diagnostics"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+					opts.desc = "Go to next diagnostics"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+					opts.desc = "Show doc for what is under cursor"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+				end,
+			})
+
 			-- Get nvim-cmp capabilities
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
